6eac9bf0260c413c0bfe08793cb240f147247d68
arnos-stuff <bcda0276@gmail.com>
2023-01-29 04:05:09 +0100

    generate css font face declarations

---
+++ gen_css.py
@@ -0,0 +1,23 @@
+import shutil
+from pathlib import Path
+
+
+def template(name, path):
+    return (
+        "@font-face {" +
+    f"\n    font-family: '{name}';" +
+    f"\n    src: url(https://github.com/arnos-stuff/fonts/raw/main/{path});" +
+    "\n}\n\n"
+    )
+
+content = []
+open('fonts.css', 'w+').write('')
+
+with open('fonts.css', 'a') as fp:
+    for font in Path(".").glob("**/*.ttf"):
+        old_name = font.name
+        new_name = font.name.replace(" ", "-")
+        newpath = Path(str(font).replace(old_name, new_name))
+        shutil.move(font, newpath)
+        font_css = template(newpath.stem, newpath).replace('\\', '/')
+        fp.write(font_css)
